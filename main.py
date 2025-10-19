347fd6b8405b3e704c8df010b253b150879bf894
michal <jan.jankowski.0690@gmail.com>
2025-10-19 19:07:36 +0200

    added necessary data formating

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from sklearn.preprocessing import StandardScaler
 
 # loading data
 data = pd.read_csv("task_data.csv")
+# formating df
+data.columns = data.columns.str.strip().str.replace(' ', '_')
+wrong_type_columns = ["CTR_-_Cardiothoracic_Ratio","Inscribed_circle_radius","Heart_perimeter"]
+for col in wrong_type_columns:
+    data[col] = data[col].astype(str).str.replace(',','.').astype(float)
+print(data)
 # data separation
 X = data[[
-    "Heart width","Lung width", "CTR - Cardiothoracic Ratio",
-    "xx","yy","xy","normalized_diff","Inscribed circle radius",
-    "Polygon Area Ratio","Heart perimeter","Heart area", "Lung area"
+    "Heart_width","Lung_width", "CTR_-_Cardiothoracic_Ratio",
+    "xx","yy","xy","normalized_diff","Inscribed_circle_radius",
+    "Polygon_Area_Ratio","Heart_perimeter","Heart_area", "Lung_area"
 ]]
 y = data["Cardiomegaly"]
 # splitting dataset into training (80%) and testing (20%)
